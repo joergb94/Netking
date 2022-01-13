1b45e81202a8221440100c9193fbace0146c3b3d
Jorge Andres Puerto Loeza <andres.puerto@dedicatedemail.us>
2022-01-13 16:42:57 -0600

    purchase membership

--- app/Repositories/GeneralRepository.php
+++ app/Repositories/GeneralRepository.php
@@ -8,6 +8,7 @@ use App\Models\Card_detail_network;
 use App\Models\Cards_style_detail;
 use App\Models\CardUserDetail;
 use App\Models\Membership;
+use App\Models\Type_user;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
@@ -26,5 +27,25 @@ class GeneralRepository {
         $status = ($total_cards >= $maxCard)? false : true;
         
         return $status;
+    }
+    public function purchase_transaction($transaction,$new_membership,$old_membershit,$id,$type_user_id)
+    {
+        $status = (isset($transaction))? $transaction : false;
+        if($status)
+        {
+            $change = Membership::where('user_id',$id)
+                ->where('type_membershit',$old_membershit)
+                ->first();
+            $type_user = Type_user::find($type_user_id);
+               return DB::transaction(function () use ($change, $new_membership,$type_user){
+                    if(
+                        $change->update([
+                            'type_user_id' => $type_user->id,
+                            'quantity' => $type_user->max_cards,
+                            'type_membership_id' => $new_membership
+                        ])
+                    ){}
+                });
+        }
     }
 }
