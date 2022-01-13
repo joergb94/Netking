1234e1259e6703c8be70de2aab18cf1b3d934b1b
JoseRGB <joseraymundogamboa30@gmail.com>
2022-01-13 11:55:21 -0600

    add style and mode delete

--- resources/views/Cards/items/header-buttons.blade.php
+++ resources/views/Cards/items/header-buttons.blade.php
@@ -1,5 +1,21 @@
 <div class="btn-toolbar float-right" role="toolbar" aria-label="@lang('labels.general.toolbar_btn_groups')">
-  <button class="btn btn-success ml-1 btn-create" onclick="Cards.create()" data-toggle="tooltip">
-    Create
-    <i class='fas fa-plus'></i></button>
+  <div class="col-sm-12">
+    <div class="row">
+        <div class="col-sm-6">
+            <span class="switch switch-sm">
+                <input type="checkbox" class="switch" onchange="transactions.mode_delete()" id="idChk-sm">
+                <label for="idChk-sm"></label>
+            </span>
+            <div class="col-sm-12">
+              modo borrar
+            </div>
+           
+        </div>
+        <div class="col-sm-6">
+            <button class="btn btn-success ml-1 btn-create" onclick="Cards.create()" data-toggle="tooltip">
+               Create
+            <i class='fas fa-plus'></i></button>
+        </div>
+    </div>
+  </div>
 </div>
