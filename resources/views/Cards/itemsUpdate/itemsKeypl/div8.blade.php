0e366a4481f14a7884d9ce144b673cfb98380ebe
joergb94 <67659588+joergb94@users.noreply.github.com>
2022-01-19 16:22:53 -0600

    changes style keypls

--- resources/views/Cards/itemsUpdate/itemsKeypl/div8.blade.php
+++ resources/views/Cards/itemsUpdate/itemsKeypl/div8.blade.php
@@ -1,5 +1,9 @@
 <div class="row justify-content-between">
-    <a href="{{$ci['card_detail']['description']}}" download>
-          {{$ci['card_detail']['name']}}
-    </a>
+    <div class="col-12 text-center mx-auto d-block">
+        <a href="{{$ci['card_detail']['description']}}" class="btn btn-gray text-white btn-block" download>
+            <div class="col-12">
+                <h3>{{$ci['card_detail']['name']}} <i class="fa fa-file-download"></i></h3>
+            </div>
+        </a>
+    </div>
 </div>
