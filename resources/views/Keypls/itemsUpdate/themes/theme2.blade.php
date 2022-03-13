0045a6afe1c9ec54b45e2d48a9fb659cfe29bfef
jose.gamboa <jose.gamboa@dedicatedpeople.us>
2022-03-12 19:16:27 -0600

    correction to template #2

--- resources/views/Keypls/itemsUpdate/themes/theme2.blade.php
+++ resources/views/Keypls/itemsUpdate/themes/theme2.blade.php
@@ -8,13 +8,13 @@
                         </div>
                     @endif
                     @if(isset($cardItems[1]))
-                        <div class="col-12 theme2-padding theme-2" id="div-{{$cardItems[1]['card_detail']->id}}">
+                        <div class="col-12 theme2-padding-social theme-2" id="div-{{$cardItems[1]['card_detail']->id}}">
                              @include('Keypls.itemsUpdate.itemsKeypl.div'.$cardItems[1]['item']->id,['ci' => $cardItems[1],'template'=>200,'theme_shape'=>'theme2-shape'])
                         </div>
                     @endif
                     <div class="col-12 theme2-padding theme-2 text-center">
                         
-                            <button type="button" class="mx-auto d-block btn {{$card_style['button_style'] == 0? 'keypl-btn ':'keypl-btn-full'}} btn-sm" value="{{$friend?1:0}}" id="btn-follow">
+                            <button type="button" class="mx-auto d-block btn {{$card_style['button_style'] == 0? 'keypl-btn ':'keypl-btn-full'}}  theme-2-btn-rounded" value="{{$friend?1:0}}" id="btn-follow">
                                 @include('Keypls.itemsUpdate.themes.button')
                             </button>
                     </div> 
@@ -22,13 +22,13 @@
                     <div class="col-6 theme-2" >
                         <div class="row theme-2">
                             @if(isset($cardItems[2]))
-                                <div class="col-12 theme2-padding theme2-col" id="div-{{$cardItems[2]['card_detail']->id}}">
-                                    @include('Keypls.itemsUpdate.itemsKeypl.div'.$cardItems[2]['item']->id,['ci' => $cardItems[2],'template'=>200,'theme_shape'=>'theme2-shape'])
+                                <div class="col-12 theme2-padding theme2-col-md" id="div-{{$cardItems[2]['card_detail']->id}}">
+                                    @include('Keypls.itemsUpdate.itemsKeypl.div'.$cardItems[2]['item']->id,['ci' => $cardItems[2],'template'=>180,'theme_shape'=>'theme2-shape'])
                                 </div>
                             @endif
                             @if(isset($cardItems[3]))
-                                <div class="col-12 theme2-padding theme2-col" id="div-{{$cardItems[3]['card_detail']->id}}">
-                                    @include('Keypls.itemsUpdate.itemsKeypl.div'.$cardItems[3]['item']->id,['ci' => $cardItems[3],'template'=>200,'theme_shape'=>'theme2-shape'])
+                                <div class="col-12 theme2-padding theme2-col-md" id="div-{{$cardItems[3]['card_detail']->id}}">
+                                    @include('Keypls.itemsUpdate.itemsKeypl.div'.$cardItems[3]['item']->id,['ci' => $cardItems[3],'template'=>180,'theme_shape'=>'theme2-shape'])
                                 </div>
                             @endif
                         </div>
@@ -39,7 +39,7 @@
                         <div class="row theme-2">
                             @if(isset($cardItems[4]))
                                 <div class="col-12 theme2-padding theme2-col-sm" id="div-{{$cardItems[4]['card_detail']->id}}">
-                                    @include('Keypls.itemsUpdate.itemsKeypl.div'.$cardItems[4]['item']->id,['ci' => $cardItems[4],'template'=>95,'theme_shape'=>'theme2-shape'])
+                                    @include('Keypls.itemsUpdate.itemsKeypl.div'.$cardItems[4]['item']->id,['ci' => $cardItems[4],'template'=>'100%','theme_shape'=>'theme2-shape'])
                                 </div>
                             @endif
                             @if(isset($cardItems[5]))
@@ -49,7 +49,7 @@
                             @endif
                             @if(isset($cardItems[6]))
                                 <div class="col-12 theme2-padding theme2-col-sm" id="div-{{$cardItems[6]['card_detail']->id}}">
-                                    @include('Keypls.itemsUpdate.itemsKeypl.div'.$cardItems[6]['item']->id,['ci' => $cardItems[6],'template'=>95,'theme_shape'=>'theme2-shape'])
+                                    @include('Keypls.itemsUpdate.itemsKeypl.div'.$cardItems[6]['item']->id,['ci' => $cardItems[6],'template'=>'100%','theme_shape'=>'theme2-shape'])
                                 </div>
                             @endif
                         </div>
