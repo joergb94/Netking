5ca97afffd41b53240c55b776595e0d75dafbc5f
GerardoAguiar <79877702+GerardoAguiar@users.noreply.github.com>
2022-05-09 11:48:42 -0500

    Changes on theme 3-4
    -add changes on theme 4
    -add changes on theme 3

--- resources/views/Keypls/itemsUpdate/themes/theme3.blade.php
+++ resources/views/Keypls/itemsUpdate/themes/theme3.blade.php
@@ -6,38 +6,32 @@
                             @include('Keypls.itemsUpdate.themes.button')
                         </button>
                 </div>
-                @if(isset($cardItems[0]))
-          
+                @if(isset($cardItems[0] && isset($cardItems[0]['item']))          
                     <div class="col-6 no-margin" id="div-{{$cardItems[0]['card_detail']->id}}">
                              @include('Keypls.itemsUpdate.itemsKeypl.div'.$cardItems[0]['item']->id,['ci' => $cardItems[0],'template'=>250,'theme_shape'=>'theme3-shape1 float-right'])
                     </div>
-             
                 @endif
-                @if(isset($cardItems[1]))
+                @if(isset($cardItems[1] && isset($cardItems[1]['item']))
                     <div class="col-6 no-margin" id="div-{{$cardItems[1]['card_detail']->id}}">
                             @include('Keypls.itemsUpdate.itemsKeypl.div'.$cardItems[1]['item']->id,['ci' => $cardItems[1],'template'=>250,'theme_shape'=>'theme3-shape1 float-left'])
-            
                     </div>
                 @endif
-                @if(isset($cardItems[2]))
-                <div class="col-6 no-margin" id="div-{{$cardItems[2]['card_detail']->id}}">
+                @if(isset($cardItems[2] && isset($cardItems[2]['item']))
+                    <div class="col-6 no-margin" id="div-{{$cardItems[2]['card_detail']->id}}">
                         @include('Keypls.itemsUpdate.itemsKeypl.div'.$cardItems[2]['item']->id,['ci' => $cardItems[2],'template'=>250,'theme_shape'=>'theme3-shape2 float-left'])
-        
-                </div>
+                    </div>
                 @endif
-                @if(isset($cardItems[3]))
+                @if(isset($cardItems[3] && isset($cardItems[3]['item']))
                 <div class="col-6 no-margin" id="div-{{$cardItems[3]['card_detail']->id}}">
-                    
                         @include('Keypls.itemsUpdate.itemsKeypl.div'.$cardItems[3]['item']->id,['ci' => $cardItems[3],'template'=>250,'theme_shape'=>'theme3-shape1 float-right'])
                 </div>
                 @endif
-                @if(isset($cardItems[4]))
+                @if(isset($cardItems[4] && isset($cardItems[4]['item']))
                 <div class="col-10 no-margin" id="div-{{$cardItems[4]['card_detail']->id}}">
                         @include('Keypls.itemsUpdate.itemsKeypl.div'.$cardItems[4]['item']->id,['ci' => $cardItems[4],'template'=>250,'theme_shape'=>'theme3-shape3'])
-        
                 </div>
                 @endif
-                @if(isset($cardItems[5]))
+                @if(isset($cardItems[5] && isset($cardItems[5]['item']))
                 <div class="col-2" id="div-{{$cardItems[3]['card_detail']->id}}">
                         @include('Keypls.itemsUpdate.itemsKeypl.div'.$cardItems[5]['item']->id,['ci' => $cardItems[5],'template'=>250,'theme_shape'=>'theme3-shape1'])
                 </div>
@@ -46,25 +40,22 @@
                     <div class="row">
                         <div class="col-6">
                             <div class="row">
-                                @if(isset($cardItems[6]))
+                                @if(isset($cardItems[6] && isset($cardItems[6]['item']))
                                     <div class="col-12" id="div-{{$cardItems[6]['card_detail']->id}}">
                                             @include('Cards.itemsUpdate.itemsKeypl.div'.$cardItems[6]['item']->id,['ci' => $cardItems[6],'template'=>250,'theme_shape'=>'theme3-shape4 float-left'])
-                            
                                     </div>
                                 @endif
-                                @if(isset($cardItems[7]))
+                                @if(isset($cardItems[7] && isset($cardItems[7]['item']))
                                     <div class="col-12" id="div-{{$cardItems[7]['card_detail']->id}}">
                                             @include('Cards.itemsUpdate.itemsKeypl.div'.$cardItems[7]['item']->id,['ci' => $cardItems[7],'template'=>250,'theme_shape'=>'theme3-shape4 float-right'])
-                            
                                     </div>
                                 @endif
                            </div>
                         </div>
                         <div class="col-6  no-margin theme{{$data['themes_id']}}-padding">
-                                @if(isset($cardItems[8]))
+                                @if(isset($cardItems[8] && isset($cardItems[8]['item']))
                                     <div class="col-12" id="div-{{$cardItems[8]['card_detail']->id}}">
                                             @include('Cards.itemsUpdate.itemsKeypl.div'.$cardItems[8]['item']->id,['ci' => $cardItems[8],'template'=>250,'theme_shape'=>'theme3-shape2 float-left'])
-                            
                                     </div>
                                 @endif
                         </div>
@@ -78,25 +69,22 @@
                     <div class="row">
                         <div class="col-6">
                             <div class="row">
-                                @if(isset($cardItems[$i+1]))
+                                @if(isset($cardItems[$i+1] && isset($cardItems[$i+1]['item']))
                                     <div class="col-12" id="div-{{$cardItems[$i+1]['card_detail']->id}}">
                                             @include('Cards.itemsUpdate.itemsKeypl.div'.$cardItems[$i+1]['item']->id,['ci' => $cardItems[$i+1],'template'=>250,'theme_shape'=>'theme3-shape4 float-left'])
-                            
                                     </div>
                                 @endif
-                                @if(isset($cardItems[$i+2]))
+                                @if(isset($cardItems[$i+2] && isset($cardItems[$i+2]['item']))
                                     <div class="col-12" id="div-{{$cardItems[$i+2]['card_detail']->id}}">
                                             @include('Cards.itemsUpdate.itemsKeypl.div'.$cardItems[$i+2]['item']->id,['ci' => $cardItems[$i+2],'template'=>250,'theme_shape'=>'theme3-shape4 float-right'])
-                            
                                     </div>
                                 @endif
                            </div>
                         </div>
                         <div class="col-6  no-margin theme{{$data['themes_id']}}-padding">
-                                @if(isset($cardItems[$i+3]))
+                                @if(isset($cardItems[$i+3] && isset($cardItems[$i+3]['item']))
                                     <div class="col-12" id="div-{{$cardItems[2]['card_detail']->id}}">
                                             @include('Cards.itemsUpdate.itemsKeypl.div'.$cardItems[$i+3]['item']->id,['ci' => $cardItems[$i+3],'template'=>250,'theme_shape'=>'theme3-shape2 float-left'])
-                            
                                     </div>
                                 @endif
                         </div>
