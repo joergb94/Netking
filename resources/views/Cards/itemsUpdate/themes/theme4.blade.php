246e9f379387ad2b6ddf2cede62e470ea4e8724b
GerardoAguiar <79877702+GerardoAguiar@users.noreply.github.com>
2022-05-09 13:18:11 -0500

    Changes theme4

--- resources/views/Cards/itemsUpdate/themes/theme4.blade.php
+++ resources/views/Cards/itemsUpdate/themes/theme4.blade.php
@@ -4,26 +4,26 @@
                 @if(isset($ci['item']))
                     @if($ci['item']->id == 2)
 
-                        <div class="col-12  theme4-padding" id="drop{{$ci['card_detail']->id}}" ondrop="dragAndDrop.drop(event)" ondragover="dragAndDrop.allowDrop(event)">
+                        <div class="dashed col-12  theme4-padding" id="drop{{$ci['card_detail']->id}}" ondrop="dragAndDrop.drop(event)" ondragover="dragAndDrop.allowDrop(event)">
                             <div class="drag" draggable="true" ondragstart="dragAndDrop.drag(event)"  id="drag{{$ci['card_detail']->id}}">
                                 <button class="btn btn-warning  btn-circle top-right btn-update" data-toggle="tooltip" title="Editar Keypl!" onclick="Cards.edit_detail({{$ci['card_detail']->id}})"><i class='fas fa-edit'></i></button>
                                 <button type="button" class="btn btn-danger btn-circle top-right btn-delete-item" style="display:none" onclick="Cards.delete_item({{$ci['card_detail']->id}},{{$ci['card_detail']->card_id}})" > <i class="fa fa-trash"></i></button>
                                     @if($ci['item']->id == 5)
                                         @include('Cards.itemsUpdate.itemsKeypl.div'.$ci['item']->id,['ci' => $ci,'template'=>$ci['card_detail']['item_data'],'theme_shape'=>'theme4-shape4 mx-auto d-block'])
                                     @else
-                                        @include('Cards.itemsUpdate.itemsKeypl.div'.$ci['item']->id,['ci' => $ci,'template'=>'100%','theme_shape'=>'theme4-shape4 mx-auto d-block'])
+                                        @include('Cards.itemsUpdate.itemsKeypl.div'.$ci['item']->id,['ci' => $ci,'template'=>230,'theme_shape'=>'theme4-shape4 mx-auto d-block'])
                                     @endif
                             </div>
                         </div>
                     @else
-                        <div class="col-12 theme4-col-12 theme4-padding" id="drop{{$ci['card_detail']->id}}" ondrop="dragAndDrop.drop(event)" ondragover="dragAndDrop.allowDrop(event)">
+                        <div class="dashed col-12 theme4-col-12 theme4-padding" id="drop{{$ci['card_detail']->id}}" ondrop="dragAndDrop.drop(event)" ondragover="dragAndDrop.allowDrop(event)">
                             <div class="drag" draggable="true" ondragstart="dragAndDrop.drag(event)"  id="drag{{$ci['card_detail']->id}}">
                                 <button class="btn btn-warning  btn-circle top-right btn-update" data-toggle="tooltip" title="Editar Keypl!" onclick="Cards.edit_detail({{$ci['card_detail']->id}})"><i class='fas fa-edit'></i></button>
                                 <button type="button" class="btn btn-danger btn-circle top-right btn-delete-item" style="display:none" onclick="Cards.delete_item({{$ci['card_detail']->id}},{{$ci['card_detail']->card_id}})" > <i class="fa fa-trash"></i></button>
                                     @if($ci['item']->id == 5)
                                         @include('Cards.itemsUpdate.itemsKeypl.div'.$ci['item']->id,['ci' => $ci,'template'=>$ci['card_detail']['item_data'],'theme_shape'=>'theme4-shape4 mx-auto d-block'])
                                     @else
-                                        @include('Cards.itemsUpdate.itemsKeypl.div'.$ci['item']->id,['ci' => $ci,'template'=>'100%','theme_shape'=>'theme4-shape4 mx-auto d-block'])
+                                        @include('Cards.itemsUpdate.itemsKeypl.div'.$ci['item']->id,['ci' => $ci,'template'=>230,'theme_shape'=>'theme4-shape4 mx-auto d-block'])
                                     @endif
                             </div>
                         </div>
